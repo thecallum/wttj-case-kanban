b1f726bf5c9a7518e53453b59574ceaba76694b6
Callum Macpherson <callummac@protonmail.com>
2025-01-14 17:34:28 +0000

    update test database config

--- config/test.exs
+++ config/test.exs
@@ -6,8 +6,8 @@ import Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :wttj, Wttj.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "admin",
+  password: "root",
   hostname: "localhost",
   database: "wttj_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
