9eb57bcd5db53a2c90216c3878878be266c82c51
Callum Macpherson <callummac@protonmail.com>
2025-01-15 07:47:26 +0000

    install dependencies

--- mix.exs
+++ mix.exs
@@ -56,7 +56,9 @@ defmodule Wttj.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:absinthe, "~> 1.7"},
+      {:absinthe_plug, "~> 1.5"}
     ]
   end
 
