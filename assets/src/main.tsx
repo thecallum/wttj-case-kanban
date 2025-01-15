b889da8d72737ff25dd4b5988f681dae5ac5abd5
Callum Macpherson <callummac@protonmail.com>
2025-01-15 08:50:48 +0000

    Install Apollo client

--- assets/src/main.tsx
+++ assets/src/main.tsx
@@ -2,13 +2,36 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from 'react-query'
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client'
 
 const queryClient = new QueryClient()
 
-createRoot(document.getElementById('root')!).render(
+const apolloClient = new ApolloClient({
+  uri: 'http://localhost:4000/api/graphql/',
+  cache: new InMemoryCache(),
+})
+
+apolloClient
+  .query({
+    query: gql`
+      query GetJobs {
+        jobs {
+          id
+          name
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result));
+
+const root = createRoot(document.getElementById('root')!)
+
+root.render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <ApolloProvider client={apolloClient}>
+        <App />
+      </ApolloProvider>
     </QueryClientProvider>
   </StrictMode>
 )
