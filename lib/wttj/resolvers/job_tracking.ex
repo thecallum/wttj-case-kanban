4aef9d163d3c565b6dde16d762843cebeaaa89b0
Callum Macpherson <callummac@protonmail.com>
2025-01-15 08:15:20 +0000

    Return real data from resolvers

--- lib/wttj/resolvers/job_tracking.ex
+++ lib/wttj/resolvers/job_tracking.ex
@@ -1,19 +1,22 @@
 defmodule Wttj.Resolvers.JobTracking do
+  alias Wttj.Jobs
+  alias Wttj.Candidates
+  alias Wttj.Statuses
 
   def list_jobs(_parent, _args, _resolution) do
-    jobs = []
+    jobs = Jobs.list_jobs()
 
     {:ok, jobs}
   end
 
-  def list_statuses(_parent, _args, _resolution) do
-    statuses = []
+  def list_statuses(_parent, %{job_id: job_id}, _resolution) do
+    statuses = Statuses.list_statuses(job_id)
 
     {:ok, statuses}
   end
 
-  def list_candidates(_parent, _args, _resolution) do
-    candidates = []
+  def list_candidates(_parent, %{job_id: job_id}, _resolution) do
+    candidates = Candidates.list_candidates(job_id)
 
     {:ok, candidates}
   end
