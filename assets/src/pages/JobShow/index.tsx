b5c532e64301a0ee05959feb065131ebf7e0df73
Callum Macpherson <callummac@protonmail.com>
2025-01-15 17:07:16 +0000

    Add Drag and Drop functionality to existing layout

--- assets/src/pages/JobShow/index.tsx
+++ assets/src/pages/JobShow/index.tsx
@@ -7,14 +7,14 @@ import { Candidate, Status } from '../../types'
 import CandidateCard from '../../components/Candidate'
 import { Badge } from '@welcome-ui/badge'
 import { useBoard } from '../../hooks/useBoard'
+import { DragDropContext, Draggable, Droppable } from '@hello-pangea/dnd'
 
 interface SortedCandidates {
   [key: string]: Candidate[]
 }
 
 function JobShow() {
-  const { jobId } = useParams()
-
+  const { jobId } = useParams<{ jobId: string }>()
   const { loading, error, job, candidates, statuses } = useBoard(jobId!)
 
   const sortedCandidates: SortedCandidates = useMemo(() => {
@@ -37,6 +37,10 @@ function JobShow() {
 
   if (error) return <p>Error : {error.message}</p>
 
+  const handleOnDragEnd = e => {
+    console.log('onDragEnd', { e })
+  }
+
   return (
     <>
       <Box backgroundColor="neutral-70" p={20} alignItems="center">
@@ -45,38 +49,64 @@ function JobShow() {
         </Text>
       </Box>
 
-      <Box p={20}>
-        <Flex gap={10}>
-          {statuses?.map(status => (
-            <Box
-              w={300}
-              border={1}
-              backgroundColor="white"
-              borderColor="neutral-30"
-              borderRadius="md"
-              overflow="hidden"
-            >
-              <Flex
-                p={10}
-                borderBottom={1}
+      <DragDropContext onDragEnd={handleOnDragEnd}>
+        <Box p={20}>
+          <Flex gap={10}>
+            {statuses?.map(status => (
+              <Box
+                w={300}
+                border={1}
+                backgroundColor="white"
                 borderColor="neutral-30"
-                alignItems="center"
-                justify="space-between"
+                borderRadius="md"
+                overflow="hidden"
+                key={status.id}
               >
-                <Text color="black" m={0} textTransform="capitalize">
-                  {status.label}
-                </Text>
-                <Badge>{(sortedCandidates[status.id] || []).length}</Badge>
-              </Flex>
-              <Flex direction="column" p={10} pb={0}>
-                {sortedCandidates[status.id]?.map((candidate: Candidate) => (
-                  <CandidateCard candidate={candidate} />
-                ))}
-              </Flex>
-            </Box>
-          ))}
-        </Flex>
-      </Box>
+                <Flex
+                  p={10}
+                  borderBottom={1}
+                  borderColor="neutral-30"
+                  alignItems="center"
+                  justify="space-between"
+                >
+                  <Text color="black" m={0} textTransform="capitalize">
+                    {status.label}
+                  </Text>
+                  <Badge>{(sortedCandidates[status.id] || []).length}</Badge>
+                </Flex>
+
+                <Droppable droppableId={`droppable-${status.id}`}>
+                  {provided => (
+                    <div {...provided.droppableProps} ref={provided.innerRef}>
+                      <Flex direction="column" p={10} pb={0}>
+                        {sortedCandidates[status.id]?.map((candidate: Candidate, index: number) => (
+                          <Draggable
+                            key={candidate.id}
+                            draggableId={`draggable-${candidate.id}`}
+                            index={index}
+                          >
+                            {provided => (
+                              <div
+                                ref={provided.innerRef}
+                                {...provided.draggableProps}
+                                {...provided.dragHandleProps}
+                                style={provided.draggableProps.style}
+                              >
+                                <CandidateCard candidate={candidate} />
+                              </div>
+                            )}
+                          </Draggable>
+                        ))}
+                      </Flex>
+                      {provided.placeholder}
+                    </div>
+                  )}
+                </Droppable>
+              </Box>
+            ))}
+          </Flex>
+        </Box>
+      </DragDropContext>
     </>
   )
 }
