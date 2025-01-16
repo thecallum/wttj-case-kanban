14eb7fc3c3ca6c6ce365ebee6f7c2f836a1a312b
Callum Macpherson <callummac@protonmail.com>
2025-01-16 08:07:04 +0000

    fix migration - wrong field name

--- priv/repo/migrations/20250116075326_add_display_order_field_to_candidate.exs
+++ priv/repo/migrations/20250116075326_add_display_order_field_to_candidate.exs
@@ -3,18 +3,18 @@ defmodule Wttj.Repo.Migrations.AddDisplayOrderFieldToCandidate do
 
   def change do
     alter table(:candidates) do
-      add :status_order, :string
+      add :display_order, :string
     end
 
-    create unique_index(:candidates, [:status_id, :status_order],
+    create unique_index(:candidates, [:status_id, :display_order],
       name: :candidates_status_id_display_order_index)
   end
 
   def down do
-    drop index(:candidates, [:status_id, :status_order])
+    drop index(:candidates, [:status_id, :display_order])
 
     alter table(:candidates) do
-      remove :status_order
+      remove :display_order
     end
   end
 end
