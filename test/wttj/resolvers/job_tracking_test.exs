66c8690a94777074fd5df21a2dec8c5e8382ac33
Callum Macpherson <callummac@protonmail.com>
2025-01-17 09:12:52 +0000

    Add remaining resolver tests

--- test/wttj/resolvers/job_tracking_test.exs
+++ test/wttj/resolvers/job_tracking_test.exs
@@ -6,11 +6,85 @@ defmodule Wttj.Resolvers.JobTrackingTest do
   import Wttj.StatusesFixtures
   import Wttj.CandidatesFixtures
 
-  describe "move_candidate/3" do
-    # These tests should probably be mocked, instead of calling the database
-    test "returns error when candidate not found" do
+  # These tests should probably be mocked, instead of calling the database
+
+  describe "get_job/3" do
+    test "returns error when job not found" do
       # Arrange
 
+      # Act
+      result = JobTracking.get_job(nil, %{job_id: 100}, nil)
+
+      # Assert
+      assert {:error, "Job not found"} = result
+    end
+
+    test "returns ok when job found" do
+      # Arrange
+      job = job_fixture()
+
+      # Act
+      result = JobTracking.get_job(nil, %{job_id: job.id}, nil)
+
+      # Assert
+      assert {:ok, job} == result
+      # assert
+    end
+  end
+
+  describe "list_jobs/3" do
+    test "returns ok when jobs found" do
+      # Arrange
+      job1 = job_fixture()
+      job2 = job_fixture()
+      job3 = job_fixture()
+
+      # Act
+      result = JobTracking.list_jobs(nil, nil, nil)
+
+      # Assert
+      assert {:ok, [job1, job2, job3]} = result
+    end
+  end
+
+  describe "list_statuses/3" do
+    test "returns ok when statuses found" do
+      # Arrange
+      job1 = job_fixture()
+      job2 = job_fixture()
+
+      status1 = status_fixture(%{job_id: job1.id})
+      status2 = status_fixture(%{job_id: job1.id})
+      status3 = status_fixture(%{job_id: job2.id})
+
+      # Act
+      result = JobTracking.list_statuses(nil, %{job_id: job1.id}, nil)
+
+      # Assert
+      assert {:ok, [status1, status2]} = result
+    end
+  end
+
+  describe "list_candates/3" do
+    test "returns ok when candidates found" do
+      # Arrange
+      job1 = job_fixture()
+      status1 = status_fixture(%{job_id: job1.id})
+
+      candidate1 =
+        candidate_fixture(%{job_id: job1.id, display_order: "1", status_id: status1.id})
+
+      # Act
+      result = JobTracking.list_candidates(nil, %{job_id: job1.id}, nil)
+
+      # Assert
+      assert {:ok, [candidate1]} = result
+    end
+  end
+
+  describe "move_candidate/3" do
+    test "returns error when candidate not found" do
+      # Arrange
       args = %{
         candidate_id: 100
       }
