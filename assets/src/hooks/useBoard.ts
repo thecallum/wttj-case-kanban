94fa56fbf0de9a67c40351f8a59e85f0bcb96026
Callum Macpherson <callummac@protonmail.com>
2025-01-17 18:38:20 +0000

    Add state to useBoard, so state can be updated

--- assets/src/hooks/useBoard.ts
+++ assets/src/hooks/useBoard.ts
@@ -1,6 +1,7 @@
 import { useQuery } from '@apollo/client'
 import { GET_BOARD } from '../graphql/queries/getBoard'
 import { Candidate, Job, Status } from '../types'
+import { useEffect, useState } from 'react'
 
 export const useBoard = (jobId: string) => {
   const { loading, error, data } = useQuery<{
@@ -11,11 +12,22 @@ export const useBoard = (jobId: string) => {
     variables: { jobId },
   })
 
+  useEffect(() => {
+    setJob(() => data?.job ?? null)
+    setCandidates(() => data?.candidates || [])
+    setStatuses(() => data?.statuses || [])
+  }, [data])
+
+  const [job, setJob] = useState<Job | null>(null)
+  const [candidates, setCandidates] = useState<Candidate[]>([])
+  const [statuses, setStatuses] = useState<Status[]>([])
+
   return {
     loading,
     error,
-    job: data?.job || null,
-    candidates: data?.candidates || [],
-    statuses: data?.statuses || [],
+
+    job,
+    candidates,
+    statuses,
   }
 }
