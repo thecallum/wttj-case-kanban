a2db88a481abdbe741d1437068e21440864aa3d8
Callum Macpherson <callummac@protonmail.com>
2025-01-20 10:07:21 +0000

    cleanup

--- config/config.exs
+++ config/config.exs
@@ -36,8 +36,6 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
-# config :absinthe, :schema, Wttj.Schema
-
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
